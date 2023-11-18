ad9604ce405796968b7402d87d50dad52ee4171f
AlvaroHJ21 <alvarohuaysara@gmail.com>
2023-11-18 10:13:37 -0500

    update subtitle

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ function App() {
   const { turn, winner, draw, cells, initGame, chooseCell } = useTictactoe();
 
   return (
-    <main className='min-h-screen grid place-content-center'>
+    <main className="min-h-screen grid place-content-center">
       <h1 className="text-center uppercase my-4">Tic tac toe</h1>
-      {!(winner || draw) ? (
-        <h2 className="text-center text-cyan-600 mb-4 font-bold">Turno: {turn ? 'X' : 'O'}</h2>
-      ) : null}
+      <h2 className="text-center text-cyan-600 mb-4 font-bold">
+        {!(winner || draw) ? `Turno: ${turn ? 'X' : 'O'}` : "Fin del juego"}
+      </h2>
       <div className="border w-[300px] h-[300px] md:w-[480px] md:h-[480px] m-auto">
         <div className="grid grid-cols-3 grid-rows-3">
           {cells.map((cell, index) => (
